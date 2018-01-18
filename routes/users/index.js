1cefc4be4340af405ab8f361fdf2ec3488ed3e88
lelbil <billelattouchi@yahoo.fr>
2018-01-18 13:14:38 +0100

    add session on user signup

--- routes/users/index.js
+++ routes/users/index.js
@@ -22,7 +22,9 @@ router.post('/', async ctx => {
     const newUser = new User(Object.assign(user, { password: passwordHash }))
 
     try {
-        await newUser.save()
+        const dbUser = await newUser.save()
+        ctx.session.userId = dbUser._id
+        ctx.status = 201
     } catch (error) {
         if (error.code === 11000) {
             if (error.index === 0) throw { name: ERRORS.EXISTING_EMAIL_ERROR, message: 'This email already exists on our database', }
@@ -30,8 +32,6 @@ router.post('/', async ctx => {
         }
         throw error
     }
-
-    ctx.status = 201
 })
 
 router.post('/login', async ctx => {
